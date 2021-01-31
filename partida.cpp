9f967761f19689b808aeafa165bbaff25558e96d
Fernando Cesar <cmnando.silva@gmail.com>
2021-01-31 18:53:18 -0300

    Program finished

--- partida.cpp
+++ partida.cpp
@@ -1,52 +1,82 @@
 #include "Partida.hpp"
 #include "Jogador.hpp"
+#include <vector>
 
-Partida::Partida(int num_jogadores){
+Partida::Partida(int num_jogadores)
+{
     this->_num_jogadores = num_jogadores;
     this->_jogadores = new Jogador[num_jogadores];
     this->_atualSize = 0;
 }
-void Partida::addJogadorCarta(string nomeJogador, int numero_pontos_carta, string naipe){
-    bool existeJogador = false;  
+void Partida::addJogadorCarta(string nomeJogador, int numero_pontos_carta, string naipe)
+{
+    bool existeJogador = false;
     Carta cartaAtual(numero_pontos_carta, naipe);
-    for (int i=0;i<this->_num_jogadores;i++){
-        if (_jogadores[i].getNomeJogador() == nomeJogador){
+    for (int i = 0; i < this->_num_jogadores; i++)
+    {
+        if (_jogadores[i].getNomeJogador() == nomeJogador)
+        {
             _jogadores[i].adicionaCarta(cartaAtual);
-            cout << "Nova carta adicionada!!!" << endl;
             break;
         }
-        else if (i == this->_num_jogadores-1){
-            cout << "Novo jogador criado!!!" << endl;
+        else if (i == this->_num_jogadores - 1)
+        {
             _jogadores[this->_atualSize].setNomeJogador(nomeJogador);
             _jogadores[this->_atualSize].adicionaCarta(cartaAtual);
             this->_atualSize++;
         }
     }
 }
-int Partida::getNumJogadores(){
+int Partida::getNumJogadores()
+{
     return this->_num_jogadores;
 }
-int Partida::getNumAtualJogadores(){
+int Partida::getNumAtualJogadores()
+{
     return this->_atualSize;
 }
-void Partida::setJogadores(Jogador* jogadores){
+void Partida::setJogadores(Jogador *jogadores)
+{
     this->_jogadores = jogadores;
 }
-Jogador* Partida::getJogadores(){
+Jogador *Partida::getJogadores()
+{
     return this->_jogadores;
 }
-Jogador Partida::getCampeao(){
+Jogador Partida::getCampeao()
+{
     int indiceMenor = 0;
-    return this->_jogadores[0];
+    for (int i = 1; i < this->_atualSize; i++)
+    {
+        if (this->_jogadores[i].calcularPontuacao() > this->_jogadores[indiceMenor].calcularPontuacao())
+        {
+            indiceMenor = i;
+        }
+    }
+    return this->_jogadores[indiceMenor];
 }
-Jogador* Partida::getJogadoresOrdenados(){
+Jogador *Partida::getJogadoresOrdenados()
+{
     return this->_jogadores;
 }
-void Partida::imprimeJogadoresOrdenados(){
-    for (int i=0;i<this->_atualSize;i++){
-        cout << endl << this->_jogadores[i].getNomeJogador() << " " << this->_jogadores[i].calcularPontuacao();
+void Partida::imprimeJogadoresOrdenados()
+{
+    vector<Jogador> aux;
+    for (int i = 0; i < this->_atualSize; i++)
+    {
+        aux.push_back(this->_jogadores[i]);
+    }
+    for (int i = 0; aux.size() > 0; i++)
+    {
+        int indiceMenor = 0;
+        for (int j = 0; j < aux.size(); j++)
+        {
+            if (aux[j].calcularPontuacao() < aux[indiceMenor].calcularPontuacao())
+            {
+                indiceMenor = j;
+            }
+        }
+        cout << aux[indiceMenor].getNomeJogador() << " " << aux[indiceMenor].calcularPontuacao() << endl;
+        aux.erase(aux.begin() + indiceMenor);
     }
 }
-/*
-	~Partida();
-	*/
